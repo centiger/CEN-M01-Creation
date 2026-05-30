c0e3543b34a44bb2e08f75c360edaf625a5b0bae
centiger <centiger2@gmail.com>
2026-05-30 15:44:38 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE = 'creation-matrix-game-v1';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./assets/creation-bg.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE='creation-matrix-layered-v1';const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/creation-bg-clean.jpg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
