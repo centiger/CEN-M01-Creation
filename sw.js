ef9efeaedba5381d18cc06c2a36f96cb38187679
centiger <centiger2@gmail.com>
2026-05-31 21:28:38 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE='creation-judgment-hub-v1';
+const CACHE='creation-babel-hub-v1';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./bg-approved.png','./manifest.webmanifest'])));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+
+/* babel-hub-1 */
