658f77bb80e9eb16dc0615ccc0760a9d5ab6a17a
centiger <centiger2@gmail.com>
2026-06-15 21:20:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
-const CACHE='cen-m01-creation-standalone-fix-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./bg-approved.png','./icon.svg'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.includes('creation')||k.includes('cen-m01')).filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request))));
+const CACHE='cen-m01-creation-installable-v20260615a';
+const ASSETS=[
+  '/CEN-M01-Creation/',
+  '/CEN-M01-Creation/index.html',
+  '/CEN-M01-Creation/manifest.json',
+  '/CEN-M01-Creation/manifest.webmanifest',
+  '/CEN-M01-Creation/icons/icon-192.png',
+  '/CEN-M01-Creation/icons/icon-512.png'
+];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.includes('cen-m01-creation')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r;}).catch(()=>caches.match(e.request)));});
