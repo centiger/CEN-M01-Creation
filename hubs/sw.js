0806b70e437ccf5b04bbad40eddd397ceed0864b
centiger <centiger2@gmail.com>
2026-06-16 10:18:18 +0900

    Add files via upload

--- hubs/sw.js
+++ hubs/sw.js
@@ -1,2 +1,4 @@
 self.addEventListener('fetch',e=>{});
 /* abraham-hub-1 */
+
+/* 20260616-next-era-links */
--- sw.js
+++ sw.js
@@ -1,12 +1,6 @@
-const CACHE='cen-m01-creation-installable-v20260615a';
-const ASSETS=[
-  '/CEN-M01-Creation/',
-  '/CEN-M01-Creation/index.html',
-  '/CEN-M01-Creation/manifest.json',
-  '/CEN-M01-Creation/manifest.webmanifest',
-  '/CEN-M01-Creation/icons/icon-192.png',
-  '/CEN-M01-Creation/icons/icon-512.png'
-];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.includes('cen-m01-creation')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r;}).catch(()=>caches.match(e.request)));});
+const CACHE='creation-matrix-precision-v1';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./bg-approved.png','./manifest.webmanifest'])));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+
+/* 20260616-next-era-links */
