97db2a2e54e609efaf0bce1e1f1cdba5ee78777c
centiger <centiger2@gmail.com>
2026-06-16 21:02:07 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,4 @@
-const CACHE='creation-matrix-meaning-panorama-v6-safe';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])));self.skipWaiting();});
+const CACHE='creation-matrix-meaning-panorama-service-v7';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./bg-approved.png','./manifest.webmanifest'])));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
-
-/* 20260616-next-era-links */
-
-/* 20260616-meaning-panorama-v6-safe */
